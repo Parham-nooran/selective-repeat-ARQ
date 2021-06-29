76647a6410a87b2fbb797d3109e2a1a880ccfadb
Parham-nooran <parhamnooranbakht@gmail.com>
2021-06-29 17:54:09 +0430

    Plotter added

--- main/SelectiveRepeatARQ/graphics/__init__.py
+++ main/SelectiveRepeatARQ/graphics/__init__.py
@@ -1,2 +1,15 @@
 import tkinter
+import matplotlib.pyplot as mp
 
+
+class Plotter:
+    def __init__(self, data):
+        self.data = data
+        self.frameLengths = self.data.keys()
+        self.times = {}
+        for key in self.frameLengths:
+            self.times[key] = sum(self.data[key])/ len(data[key])
+
+    def plot(self):
+        mp.plot(self.times.keys(), self.times.values())
+        mp.show()
